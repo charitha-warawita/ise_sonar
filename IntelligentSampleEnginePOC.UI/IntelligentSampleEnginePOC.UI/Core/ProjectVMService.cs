1065ff1d336fcef7c2dd3387966b3046d46ed2cb
mailgopalv <mailgopalv@gmail.com>
2022-05-31 09:40:53 +0100

    Cleaning up the solution

--- IntelligentSampleEnginePOC.UI/IntelligentSampleEnginePOC.UI/Core/ProjectVMService.cs
+++ IntelligentSampleEnginePOC.UI/IntelligentSampleEnginePOC.UI/Core/ProjectVMService.cs
@@ -54,51 +54,9 @@ namespace IntelligentSampleEnginePOC.UI.Core
 
         public async Task<Project> CreateProject(Project project)
         {
-            /*var project = new Project();
-            if(projectRecord != null)
-            {
-                project.Name = projectRecord.Name;
-                project.Reference = projectRecord.Reference;
-                project.User = new User()
-                {
-                    Name = projectRecord.UserName,
-                    Email = projectRecord.UserEmail
-                };
-                project.StartDate = projectRecord.StartDate;
-                project.FieldingPeriod = projectRecord.FieldingPeriod;
-                project.Status = Status.Draft;
-                project.LinkToSurvey = projectRecord.LinkToSurvey;
-                project.LastUpdate = DateTime.UtcNow;
-            }*/
+            
             StringBuilder urlBuilder = new StringBuilder();
             urlBuilder.Append(Path.Combine(_settings.Url, _settings.Path));
-
-            /*var queryString = new List<string>();
-            if (!string.IsNullOrEmpty(status))
-                // urlBuilder.Append("/status/" + EnumHelpers<Status>.Parse(status));
-                queryString.Add("status="+(int)(EnumHelpers<Status>.Parse(status)));
-            if (!string.IsNullOrEmpty(searchString))
-                // urlBuilder.Append("/searchString/" + searchString);
-                queryString.Add("searchstring="+searchString);
-
-            queryString.Add("recentCount=20");
-
-            if (queryString.Any())
-            {
-                urlBuilder.Append("?");
-                foreach (var item in queryString)
-                {
-                    urlBuilder.Append(item+"&");
-                }
-            }
-            HttpResponseMessage response = await _httpClient.GetAsync(urlBuilder.ToString());
-            var projectViewModel = new ProjectViewModel();
-            projectViewModel.Statuses = new SelectList(statusList);
-            if (response.IsSuccessStatusCode)
-            {
-                var projects = await response.Content.ReadFromJsonAsync<List<Project>>();
-                projectViewModel.Projects = projects;
-            }*/
 
             return project;
         }
