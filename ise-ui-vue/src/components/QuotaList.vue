ac11ff08099627c981df7c19e05b995ddb5cbf46
KT\YarlagaddaH <hyma.yarlagadda.hcl.kantar.com>
2022-07-26 15:48:08 +0530

    Quota ui changes

--- ise-ui-vue/src/components/QuotaList.vue
+++ ise-ui-vue/src/components/QuotaList.vue
@@ -1,4 +1,5 @@
 <template>
+ <p>ItemType: {{itemType}} ; TAId: {{ taId }} ; QID: {{ quotaid }} ; showQuotaCondition: {{ showQuotaCondition }}</p>
   <div class="container">
     <div class="row">
       <div class="col-md-6">
@@ -6,8 +7,21 @@
         <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.name" />
       </div>
       <div class="col-md-6">
-        <label for="input" class="form-label">Field Target(in percentage)</label>
-        <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.fieldTarget" />
+        <label for="sel1" class="form-label">Servey Quota Type</label>
+        <select v-model="key" @change="useQuotaDataStore.serveyQuotaTyp($event)" class="form-select" id="sel2" name="sellist1">
+         <option v-bind:value=ServeyQuotaTypes v-for="ServeyQuotaTypes in useQuotaDataStore.currentQuota.ServeyQuotaType" :key="ServeyQuotaTypes">{{ServeyQuotaTypes}}</option>
+         <label for="sel1" class="form-label">Select list (select one):</label>
+          <!-- <option>Client</option>
+          <option>Control</option>
+          <option>Supplier</option> -->
+        </select>
+      </div>
+      <div class="col-md-6">
+        <label for="sel2" class="form-label">Adjustment Type</label>
+        <select class="form-select" id="sel2" name="sellist2">
+          <option>Nominal</option>
+          <option>percentage</option>
+        </select>
       </div>
       <div class="col-md-6">
         <label for="input" class="form-label">Status</label>
@@ -18,10 +32,56 @@
         <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.completes" />
       </div>
       <div class="col-md-6">
-        <label for="input" class="form-label">Prescreence</label>
-        <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.prescreence" />
+        <label for="input" class="form-label">Prescreens</label>
+        <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.prescreens" />
+      </div>
+       <div class="col-md-6">
+        <div class="row">
+           <div class="col col-lg-4" style="margin-top: 8%;">
+              <label for="input" class="form-label">Field Target</label>
+          </div>
+          <div class="col col-lg-3">
+              <label for="input" class="form-label">Nominal</label>
+              <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.fieldTargetNominal" />
+          </div>
+          <div class="col col-lg-3">
+             <label for="input" class="form-label">Percentage(%)</label>
+             <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.fieldTargetPercentage" />
+          </div>
+          
+      </div>
       </div>
       <div class="col-md-6">
+        <div class="row">
+           <div class="col col-lg-4" style="margin-top: 8%;">
+              <label for="input" class="form-label">Quota</label>
+          </div>
+          <div class="col col-lg-3">
+              <label for="input" class="form-label">Nominal</label>
+              <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.quotaNominal" />
+          </div>
+          <div class="col col-lg-3">
+             <label for="input" class="form-label">Percentage(%)</label>
+             <input type="text" class="form-control" v-model="useQuotaDataStore.currentQuota.quotaPercentage" />
+          </div>
+          
+      </div>
+      </div>
+   
+    
+      
+       <!-- <div class="col-md-6">
+        <label for="sel1" class="form-label">Select Condition</label>
+        <select
+          class="form-select"
+          id="sel1"
+          name="sellist1"
+          v-model="key"
+          @change="useQuotaDataStore.selectQuotaCondition($event)">
+          <option></option>
+        </select>
+      </div> -->
+      <!-- <div class="col-md-6">
         <label for="sel1" class="form-label">Select Condition</label>
         <select
           class="form-select"
@@ -31,13 +91,32 @@
           @change="useQuotaDataStore.selectQuotaCondition($event)">
           <option v-bind:value=condition selected="[condition === 'None' ? 'selected' : '']"  v-for="condition in useQuotaDataStore.currentQuota.conditions" :key="condition">{{condition}}</option>
         </select>
-      </div>
+      </div> -->
     </div>
+  </div> 
+    <div class="col-md-12 p-4">
+      <button class="btn btn-outline-success searchButton" id="addQutobutton" v-on:click="useQuotaDataStore.addCondition">Add condition</button>  
+    </div>
+ 
+  <fieldset class="col-md-12 border p-2 fieldsetstyle" v-show ="useQuotaDataStore.conditiongrid">
+      <legend  class="float-none w-auto" style="font-size: 18px;">Condition</legend>
+      <div class="container">
+          <div class="row">
+               <div class="col-md-3">
+                  <label for="sel1" class="form-label">Select Condition</label>
+              </div>
+              <div class="col-md-6">
+                  <select class="form-select" id="sel1" name="sellist1" v-model="key" @change="useQuotaDataStore.selectQuotaCondition($event)">
+                  <option v-bind:value=condition selected="[condition === 'None' ? 'selected' : '']"  v-for="condition in useQuotaDataStore.currentQuota.conditions" :key="condition">{{condition}}</option>
+                  
+                  <!-- <option v-bind:value=condition selected="[condition === 'None' ? 'selected' : '']"  v-for="condition in useQuotaDataStore.currentQuota.conditions" :key="condition">{{condition}}</option> -->
+              </select>
+              </div>
+          </div>
+           <div v-if="useQuotaDataStore.showSubPopup">
+  <div class="container conditionbox" v-if="useQuotaDataStore.showQuotaCondition === 'none'">
   </div>
-  <div v-if="useQuotaDataStore.showSubPopup">
-  <div class="container" v-if="useQuotaDataStore.showQuotaCondition === 'none'">
-  </div>
-  <div class="container" v-if="useQuotaDataStore.showQuotaCondition === 'country'">
+  <div class="container conditionbox" v-if="useQuotaDataStore.showQuotaCondition === 'country'">
     <div class="row">
       <div class="col-md-12">
         <div class="card" style="margin-top: 5%">
@@ -67,7 +146,7 @@
       </div>
     </div>
   </div>
-  <div class="container" v-if="useQuotaDataStore.showQuotaCondition === 'age'">
+  <div class="container conditionbox" v-if="useQuotaDataStore.showQuotaCondition === 'age'">
     <div class="row">
       <div class="col-md-12">
         <div class="card" style="margin-top: 5%">
@@ -100,7 +179,7 @@
       </div>
     </div>
   </div>
-  <div class="container" v-if="useQuotaDataStore.showQuotaCondition === 'gender'">
+  <div class="container conditionbox" v-if="useQuotaDataStore.showQuotaCondition === 'gender'">
     <div class="row">
       <div class="col-md-12">
         <div class="card" style="margin-top: 5%">
@@ -135,7 +214,11 @@
     </div>
   </div>
   </div>
-  <div class="col-md-12">
+      </div>
+            
+  </fieldset>
+  
+  <div class="col-md-12 p-4">
     <button
       class="btn btn-outline-success searchButton me-2"
       style="width: 100%; margin: 5px 0"
@@ -149,10 +232,13 @@
 </template>
 <script setup>
 import { useQuotaStore } from "@/stores/quotaStore";
+import {useProjectStore} from '@/stores/projectStore';
 import { storeToRefs } from "pinia";
 import { onMounted } from "vue";
-const props = defineProps(["itemType", "taId", "quotaid"]);
+const props = defineProps(["itemType", "taId", "quotaid"])
+const { project} = storeToRefs(useProjStore)
 
+var useProjStore = useProjectStore();
 var useQuotaDataStore = useQuotaStore();
 
 const {
@@ -165,3 +251,9 @@ const {
   quotaMaxAge,
 } = storeToRefs(useQuotaDataStore);
 </script>
+
+<style>
+.fieldsetstyle {
+  border: 1px solid #8f959b!important;
+  }
+</style>
--- ise-ui-vue/src/stores/quotaStore.js
+++ ise-ui-vue/src/stores/quotaStore.js
@@ -3,21 +3,33 @@ import { useProjectStore } from "./projectStore";
 export const useQuotaStore = defineStore('quota', {
     state: () => ({
         currAgeRange: '',
+        quotaMinAge: 0,
+        quotaMaxAge:0,
         error: null,
         showQuotaCondition:'',
         quotaCountries:[],
         quotaGenders:[],
         showSubPopup: false,
+        conditiongrid:false,
+        iseUrl: import.meta.env.VITE_ISE_API_URL,
+        iseCountryPath: import.meta.env.VITE_ISE_API_COUNTRIES,
 
         currentQuota: {
             name: '',
-            fieldTarget: 0,
+            serveyQuotaTypeSelected:'',
+            adjustmentType:'',
+            fieldTargetPercentage: 0,
+            fieldTargetNominal:0,
             status: 0,
             completes: 0,
-            prescreence: 0,
+            prescreens: 0,
+            quotaPercentage: 0,
+            quotaNominal: 0,
             conditions: ["None", "Age", "Gender", "Country"],
+            ServeyQuotaType:["Client","Control","Supplier"],
             quotaMinAge:0,
             quotaMaxAge:0,
+            currAgeRange: '',
         }
         
 
@@ -30,55 +42,180 @@ export const useQuotaStore = defineStore('quota', {
         LoadDefaultCurrentQuota() {
             this.currentQuota = {
                 name: '',
-                fieldTarget: 0,
+                serveyQuotaTypeSelected:'',
+                adjustmentType:'',
+                fieldTargetPercentage: 0,
+                fieldTargetNominal: 0,
                 status: 0,
                 completes: 0,
-                prescreence: 0,
+                prescreens: 0,
+                quotaPercentage: 0,
+                quotaNominal: 0,
                 conditions: ["None", "Age", "Gender", "Country"],
                 showQuotaCondition:'',
+                quotaCountries:[],
                 quotaMinAge:0,
                 quotaMaxAge:0,
+                ServeyQuotaType:["Client","Control","Supplier"],
             };
             this.showSubPopup = false;
         },
-      async  GetQuota(itemtype, taId, qid) {
+        GetQuota(itemtype, taId = 1, qid = 2 ) {
             if(itemtype === 'age')
             {
+                this.GetQuotanAge(itemtype,taId, qid);
+                // var project = useProjectStore().project;
+                // for (var i = 0; i < project.projectTargetAudiences.length; i++)
+                // {
+                //     var projectQuotas=project.projectTargetAudiences
+                //     if(project.projectTargetAudiences[i].id === taId)
+                //     {
+                //         for(var j = 0; j < project.projectTargetAudiences[i].quota.length; j++)
+                //         {
+                //             if(project.projectTargetAudiences[i].quota[j].id === qid)
+                //             {
+                //                 var variable = project.projectTargetAudiences[i].quota[j].condition.variables[0];
+                //                 this.currAgeRange = variable.name;
+                //                 break;
+                //             }
+                //         }
+                //     }
+               // }
+            }
+            if(itemtype === "country")
+            {
+                this.GetQuotaCountry(itemtype, taId = 1, qid = 2);
+                // if(this.quotaCountries.length === 0)
+                // {
+                //     var countriesList = [{"id":1,"name":"UK","selected":true},{"id":2,"name":"Sweden","selected":false},{"id":3,"name":"Germany","selected":false},{"id":4,"name":"Denmark","selected":false},{"id":5,"name":"Finland","selected":false},{"id":6,"name":"Norway","selected":false},{"id":9,"name":"Spain","selected":false},{"id":10,"name":"Italy","selected":false},{"id":11,"name":"Hungary","selected":false},{"id":12,"name":"Greece","selected":false},{"id":22,"name":"USA","selected":false}];
+                //     this.quotaCountries = countriesList;
+                // }
+            }
+            if(itemtype === "gender")
+            {
+                this.GetQuotaGender(itemtype, taId =1, qid);
+                // if(this.quotaGenders.length === 0)
+                // {
+                //     console.log('Call made to Gender API');
+                //     var gendersList = [{"id":1,"name":"Male","selected":true},{"id":2,"name":"Female","selected":true}];
+                //     this.quotaGenders = gendersList;
+                // }
+            }
+        },
+        async GetQuotaCountry(itemtype, taId =1, qid =2)
+        {  console.log(itemtype,taId,qid,"Hymacountrues")
+            if(this.quotaCountries.length === 0)
+                {
+                    console.log(itemtype + '--' + taId + '--' + qid);
+                    // var countriesList = [{"id":1,"name":"UK","selected":true},{"id":2,"name":"Sweden","selected":false},{"id":3,"name":"Germany","selected":false},{"id":4,"name":"Denmark","selected":false},{"id":5,"name":"Finland","selected":false},{"id":6,"name":"Norway","selected":false},{"id":9,"name":"Spain","selected":false},{"id":10,"name":"Italy","selected":false},{"id":11,"name":"Hungary","selected":false},{"id":12,"name":"Greece","selected":false},{"id":22,"name":"USA","selected":false}];
+                    // this.countries = countriesList;
+                    //this.countriesLoading = true
+                    try {
+                        this.quotaCountries = await fetch(this.iseUrl + this.iseCountryPath)
+                        .then((response) => response.json())
+                    } catch (error) {
+                        //this.countriesError = error
+                        return;
+                    } finally {
+                        this.countriesLoading = false
+                    }
+                }
+                var currProjCountryIdList = [];
                 var project = useProjectStore().project;
                 for (var i = 0; i < project.projectTargetAudiences.length; i++)
                 {
-                    var projectQuotas=project.projectTargetAudiences
                     if(project.projectTargetAudiences[i].id === taId)
                     {
-                        for(var j = 0; j < project.projectTargetAudiences[i].quota.length; j++)
+                        for(var j = 0; j < project.projectTargetAudiences[i].qualifications.length; j++)
                         {
-                            if(project.projectTargetAudiences[i].quota[j].id === qid)
+                            if(project.projectTargetAudiences[i].qualifications[j].id === qid)
                             {
-                                var variable = project.projectTargetAudiences[i].quota[j].condition.variables[0];
-                                this.currAgeRange = variable.name;
-                                break;
+                                var currVar = project.projectTargetAudiences[i].qualifications[j].question.variables;
+                                for(var k = 0; k < currVar.length; k++)
+                                    currProjCountryIdList.push(currVar[k].id);
                             }
                         }
                     }
                 }
-            }
-            if(itemtype === "country")
-            {
-                if(this.quotaCountries.length === 0)
+                for (var i = 0; i < this.quotaCountries.length; i++)
                 {
-                    var countriesList = [{"id":1,"name":"UK","selected":true},{"id":2,"name":"Sweden","selected":false},{"id":3,"name":"Germany","selected":false},{"id":4,"name":"Denmark","selected":false},{"id":5,"name":"Finland","selected":false},{"id":6,"name":"Norway","selected":false},{"id":9,"name":"Spain","selected":false},{"id":10,"name":"Italy","selected":false},{"id":11,"name":"Hungary","selected":false},{"id":12,"name":"Greece","selected":false},{"id":22,"name":"USA","selected":false}];
-                    this.quotaCountries = countriesList;
+                    if(currProjCountryIdList.includes(this.quotaCountries[i].id))
+                        this.quotaCountries[i].selected = true;
+                    else 
+                        this.quotaCountries[i].selected = false;
+                }
+        },
+        async GetQuotanAge(itemtype, taId =1, qid =2)
+        {
+            console.log(itemtype + '--' + taId + '--' + qid);
+            var project = useProjectStore().project;
+            for (var i = 0; i < project.projectTargetAudiences.length; i++)
+            {
+                if(project.projectTargetAudiences[i].id === taId)
+                {
+                    for(var j = 0; j < project.projectTargetAudiences[i].qualifications.length; j++)
+                    {
+                        if(project.projectTargetAudiences[i].qualifications[j].id === qid)
+                        {
+                            var variable = project.projectTargetAudiences[i].qualifications[j].question.variables[0];
+                            this.currAgeRange = variable.name;
+                            var numberIndex = variable.name.indexOf(' - ');
+                            this.quotaMinAge = variable.name.substring(0, numberIndex);
+                            this.quotaMaxAge = variable.name.substring(numberIndex+3, variable.name.length);
+                            break;
+                        }
+                    }
                 }
             }
-            if(itemtype === "gender")
+        },
+        async GetQuotaGender(itemtype, taId, qid)
+        {
+            if(this.quotaGenders.length === 0)
             {
-                if(this.quotaGenders.length === 0)
-                {
-                    console.log('Call made to Gender API');
-                    var gendersList = [{"id":1,"name":"Male","selected":true},{"id":2,"name":"Female","selected":true}];
-                    this.quotaGenders = gendersList;
-                }
+                console.log('Call made to Gender API');
+                var gendersList = [{"id":1,"name":"Male","selected":false},{"id":2,"name":"Female","selected":false}];
+                this.quotaGenders = gendersList;
             }
+        },
+        // async GetQandAForCategory(profCategory)
+        // {
+        //     var category = profCategory.name
+        //     this.selectedCategory = category;
+        //     this.profCategoriesLoading = true;
+
+        //     try {
+        //         var currIndex = this.profCategories.findIndex(x => x.name === category)
+        //         if(this.profCategories[currIndex].qas.length < 1)
+        //         {
+        //             var qas = await fetch(this.iseUrl + this.iseQAByCategoryPath + encodeURIComponent(category))
+        //             .then((response) => response.json())
+
+        //             for(var i =0; i < qas.length; i++)
+        //             {
+        //                 for(var j = 0; j< qas[i].variables.length; j++)
+        //                 {
+        //                     qas[i].variables[j].selected = false;
+        //                 }
+        //             }
+                    
+        //             this.profCategories[currIndex].qas = qas;
+        //         }
+
+        //         this.categoryQAs = this.profCategories[currIndex].qas;
+
+        //     } catch (error) {
+        //         this.profCategoriesError = error;
+        //     } finally {
+        //         this.profCategoriesLoading = false;
+        //     }
+        // },
+        serveyQuotaTyp(event){
+            this.currentQuota.serveyQuotaTypeSelected = event.target.value;    
+        },
+        addCondition(){
+            this.conditiongrid = true;
+            
+
         },
         selectQuotaCondition(event){
             console.log(event.target.value.toLowerCase());
@@ -97,10 +234,15 @@ export const useQuotaStore = defineStore('quota', {
                     quota = {
                         "id": quotaid,
                         "name": this.currentQuota.name,
-                        "fieldTarget": this.currentQuota.fieldTarget,
+                        "fieldTargetNominal": this.currentQuota.fieldTargetNominal,
+                        "fieldTargetPercentage": this.currentQuota.fieldTargetPercentage,
                         "status": this.currentQuota.status,
                         "completes": this.currentQuota.completes,
-                        "prescreence":this.currentQuota.prescreence ,
+                        "prescreence":this.currentQuota.prescreens,
+                        "serveyQuotaTypeSelected": this.currentQuota.serveyQuotaTypeSelected,
+                        "adjustmentType": this.currentQuota.adjustmentType,
+                        "quotaPercentage": this.currentQuota.quotaPercentage,
+                        "quotaNominal": this.currentQuota.quotaNominal,
                         "order": quotaid,
                         "logicalDecision": "OR",
                         "NumberOfRequiredConditions": 0,
