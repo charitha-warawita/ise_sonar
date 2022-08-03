4e70247c2f21657ee9e19f117417c3e700c49b46
Anuradha Srinivasan <anuradha.srinivasan@kantar.com>
2022-08-03 13:26:42 +0100

    unitTest controller

---
+++ IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Http.Tests/Controller/ProjectControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using IntelligentSampleEnginePOC.API.Core.Interfaces;
+using IntelligentSampleEnginePOC.API.Http.Controllers;
+using Moq;
+
+namespace IntelligentSampleEnginePOC.API.Http.Tests.Controller
+{
+    public class ProjectControllerTest
+    {
+        private  Mock<IProjectService> _projectService;
+        private ProjectController _projectController;
+        public ProjectControllerTest()
+        {
+            _projectService = new Mock<IProjectService>();
+            _projectController = new ProjectController(_projectService.Object);
+        }
+
+        [Fact]
+        public void GetById_ReturnsOk()
+        {
+            var okResult =  _projectController.GetById("1");
+            Assert.NotNull(okResult);
+            Assert.Equal(200, ((Microsoft.AspNetCore.Mvc.StatusCodeResult)okResult).StatusCode);
+        }
+
+        [Fact]
+        public void GetAll_ReturnsOk()
+        {
+            var okResult = _projectController.GetAll(1,"Test",2);
+            Assert.NotNull(okResult);
+            Assert.Equal(200, ((Microsoft.AspNetCore.Mvc.StatusCodeResult)okResult).StatusCode);
+        }
+
+
+    }
+}
