0b2bdca6a9c5a4e59fbc247e98fe346306779e6d
Kieran Sunshine <kieran.sunshine@kantar.com>
2022-08-12 15:07:55 +0100

    moved dummy store to security folder

--- ise-ui/src/stores/security/AuthenticationStore.ts
+++ ise-ui/src/stores/security/AuthenticationStore.ts
@@ -5,6 +5,7 @@ export const useAuthenticationStore = defineStore('authentication', () => {
 	const isLoggedIn: Ref<boolean> = ref(false);
 	const user: Ref<object | null> = ref(null);
 
+	// TODO: Will need implementing once we know what security strategy we're taking.
 	function login(username: string, password: string) {
 		if (!username || !password) return; // TODO: Error: Incorrect details.
 
