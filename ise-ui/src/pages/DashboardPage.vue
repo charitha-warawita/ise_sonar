f8a751b8462b0e1969889519e02fc0384f5f90ce
Kieran Sunshine <kieran.sunshine@kantar.com>
2022-08-18 13:32:14 +0100

    added basic projects grid to dashboard page

--- ise-ui/src/pages/DashboardPage.vue
+++ ise-ui/src/pages/DashboardPage.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProjectsGrid from '@/components/pages/home/ProjectsGrid.vue';
+import { useProjectService } from '@/services/ProjectService';
+import type { Project } from '@/types/Project';
+import { onMounted, ref } from 'vue';
+
+const projectService = useProjectService();
+
+const projects = ref<Project[]>([]);
+
+onMounted(async () => {
+	projects.value = await projectService.GetAllAsync();
+});
+</script>
 
 <template>
-	<div></div>
+	<div class="dashboard-container-grid">
+		<div>
+			<ProjectsGrid :projects="projects" />
+		</div>
+		<div>Open Risks</div>
+		<div>Cards</div>
+	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.dashboard-container-grid {
+	display: grid;
+	grid-template-columns: 3fr 1fr;
+}
+</style>
