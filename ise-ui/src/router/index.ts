b63fbf9b07cee00cc54a3272d8d6d62e0fe63b92
Kieran Sunshine <kieran.sunshine@kantar.com>
2022-08-18 13:24:49 +0100

    created dashboard page and reorganized existing pages

--- ise-ui/src/router/index.ts
+++ ise-ui/src/router/index.ts
@@ -1,20 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '@/pages/HomePage.vue';
-import ProjectPage from '@/pages/ProjectPage.vue';
+import DashboardPage from '@/pages/DashboardPage.vue';
+import ProjectDetailsPage from '@/pages/ProjectDetailsPage.vue';
+import ProjectsPage from '@/pages/ProjectsPage.vue';
 import TargetAudiencePage from '@/pages/TargetAudiencePage.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
-			name: 'home',
-			component: HomePage,
+			name: 'dashboard',
+			component: DashboardPage,
+		},
+		{
+			path: '/projects',
+			name: 'projects',
+			component: ProjectsPage,
 		},
 		{
 			path: '/project/:id',
 			name: 'project',
-			component: ProjectPage,
+			component: ProjectDetailsPage,
 		},
 		{
 			path: '/project/:project/target-audience/:ta',
