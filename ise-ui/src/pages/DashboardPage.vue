9ad75f145d81c240a9eb2e8b895429c8bb33cd1b
Kieran Sunshine <kieran.sunshine@kantar.com>
2022-08-18 13:42:18 +0100

    added open risks grid, made use of new panel component

--- ise-ui/src/pages/DashboardPage.vue
+++ ise-ui/src/pages/DashboardPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import ProjectsGrid from '@/components/pages/home/ProjectsGrid.vue';
+import Panel from '@/components/Panel.vue';
 import { useProjectService } from '@/services/ProjectService';
 import type { Project } from '@/types/Project';
 import { onMounted, ref } from 'vue';
@@ -7,6 +8,7 @@ import { onMounted, ref } from 'vue';
 const projectService = useProjectService();
 
 const projects = ref<Project[]>([]);
+const risks = ref<Project[]>([]);
 
 onMounted(async () => {
 	projects.value = await projectService.GetAllAsync();
@@ -16,9 +18,25 @@ onMounted(async () => {
 <template>
 	<div class="dashboard-container-grid">
 		<div>
-			<ProjectsGrid :projects="projects" />
+			<Panel>
+				<template #header>
+					<span style="font-weight: 600">
+						Projects <span style="color: lightskyblue">{{ projects.length }}</span>
+					</span>
+				</template>
+
+				<ProjectsGrid :projects="projects" />
+			</Panel>
 		</div>
-		<div>Open Risks</div>
+		<div>
+			<Panel>
+				<template #header>
+					<span style="font-weight: 600">
+						Open Risks <span style="color: lightskyblue">{{ risks.length }}</span>
+					</span>
+				</template>
+			</Panel>
+		</div>
 		<div>Cards</div>
 	</div>
 </template>
@@ -27,5 +45,6 @@ onMounted(async () => {
 .dashboard-container-grid {
 	display: grid;
 	grid-template-columns: 3fr 1fr;
+	gap: 20px;
 }
 </style>
