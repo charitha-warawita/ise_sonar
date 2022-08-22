5d15089bba6500d50c23c72df85dde5d1a7a1cb7
KT\YarlagaddaH <hyma.yarlagadda.hcl.kantar.com>
2022-08-22 12:37:17 +0530

    Merging develop into my baranch

---
+++ IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Core/Data/QuotaContext.cs
@@ -0,0 +1,17 @@
+﻿using IntelligentSampleEnginePOC.API.Core.Interfaces;
+using IntelligentSampleEnginePOC.API.Core.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelligentSampleEnginePOC.API.Core.Data
+{
+    internal class QuotaContext
+    {
+    }
+}
