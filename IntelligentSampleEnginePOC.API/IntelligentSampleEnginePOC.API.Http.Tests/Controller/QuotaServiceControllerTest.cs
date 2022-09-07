a8e956163b5e1b49044a5b6c03d364f1b23379ec
KT\YarlagaddaH <hyma.yarlagadda.hcl.kantar.com>
2022-09-07 16:39:40 +0530

    Quota Servicetest update

--- IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Http.Tests/Controller/QuotaServiceControllerTest.cs
+++ IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Http.Tests/Controller/QuotaServiceControllerTest.cs
@@ -28,7 +28,7 @@ namespace IntelligentSampleEnginePOC.API.Http.Tests.Controller
             [Theory]
         [InlineData(501)]
         
-        public void GetQuestions_ReturnsOk(long qtid)
+        public void GetQuota_ReturnsOk(long qtid)
         {
             string jsonString = QuotaTest.GetQuotaJson();
             Quota quota = JsonConvert.DeserializeObject<Quota>(jsonString);
@@ -36,6 +36,58 @@ namespace IntelligentSampleEnginePOC.API.Http.Tests.Controller
             var result = _quotaService.GetQuota(qtid);
             Assert.NotNull(result);
         }
+
+
+        [Theory]
+        [InlineData(301)]
+        public void GetQuota_NotNull(long qtid)
+        {
+            string jsonString = QuotaTest.GetQuotaJson();
+            Quota quota = JsonConvert.DeserializeObject<Quota>(jsonString);
+            _quotaContext.Setup(repo => repo.GetQuota(qtid)).Returns(new QuotaTest().GetQuota(qtid, quota));
+            var index = _quotaService.GetQuota(qtid);
+            Assert.NotNull(_quotaContext.Object); 
+        }
+
+        [Theory]
+        [InlineData(301)]
+
+        public void GetQuota_Return_Exceptions(long qtid)
+        {
+
+            //throw new ArgumentNullException(nameof(qtid));
+        }
+
+        [Fact]
+        public void CreateQuota_ReturnsOk()
+        {
+            string jsonString = QuotaTest.GetQuotaJson();
+            long taid = 104;
+            long projectId = 143;
+            Quota quota = JsonConvert.DeserializeObject<Quota>(jsonString);
+            _quotaContext.Setup(repo => repo.CreateQuota(projectId, taid, quota)).Returns(new QuotaTest().CreateQuotaTest(projectId, taid, quota));
+            var result = _quotaService.CreateQuota(projectId, taid, quota);
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [InlineData(301)]
+        
+        public void DeleteQuota_ReturnsOk(long qtid)
+        {
+            string jsonString = QuotaTest.GetQuotaJson();
+            Quota quota = JsonConvert.DeserializeObject<Quota>(jsonString);
+            _quotaContext.Setup(repo => repo.DeleteQuotaFromDB(qtid)).Returns(new QuotaTest().DeleteQuotaTest(qtid,quota));
+            var result = _quotaService.DeleteQuota(qtid);
+            Assert.NotNull(result);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult);
+            Assert.Equal(200, result);
+        }
+     
+
+
+
 
     }
 }
