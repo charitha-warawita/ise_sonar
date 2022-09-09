5e9b651c264aedaea2902d0e39804a074d11dd3b
Anuradha Srinivasan <anuradha.srinivasan@kantar.com>
2022-09-09 16:40:42 +0100

    validation inital checkin

--- IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Core/Services/ProjectValidator.cs
+++ IntelligentSampleEnginePOC.API/IntelligentSampleEnginePOC.API.Core/Services/ProjectValidator.cs
@@ -1,28 +1,23 @@
 ﻿using IntelligentSampleEnginePOC.API.Core.Interfaces;
 using IntelligentSampleEnginePOC.API.Core.Model;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace IntelligentSampleEnginePOC.API.Core.Services
 {
     public class ProjectValidator : IProjectValidator
     {
-       // private ILogger _logger;
-       // public ProjectValidator(ILogger logger)
-       // {
-            //_logger = logger;
-       // }
+        // private ILogger _logger;
+        // public ProjectValidator(ILogger logger)
+        // {
+        //_logger = logger;
+        // }
 
         private List<string> errors;
         public bool IsValidated(Project project)
         {
             errors = new List<string>();
             var isValidationSuccess = true;
-            if(project == null)
+            if (project == null)
             {
                 errors.Add("Project return is null");
                 isValidationSuccess = true;
@@ -30,16 +25,13 @@ namespace IntelligentSampleEnginePOC.API.Core.Services
 
             if (project != null)
             {
-                //Code review and correction required 
-
-                // ValidateProjectCategories(project);
-                // ValidateProjectTACountainsCountry(project);
+                ValidateProjectCategories(project);
                 ValidateProjectAsTA(project);
-                // ValidateProjectName(project);
-                // ValidateProjectReference(project);
-                // ValidateProjectStartDate(project);
-                // ValidateProjectFieldingPeriod(project);
-                // ValidateProjectUser(project);
+                ValidateProjectName(project);
+                ValidateProjectReference(project);
+                ValidateProjectStartDate(project);
+                ValidateProjectFieldingPeriod(project);
+                ValidateProjectUser(project);
             }
 
             if (errors.Any())
@@ -47,71 +39,53 @@ namespace IntelligentSampleEnginePOC.API.Core.Services
 
             return true;
         }
-
-        private void ValidateProjectTACountainsCountry(Project project)
-        {
-            
-        }
-        
-        
-        // Code review and correction required
         private void ValidateProjectName(Project project)
         {
-            if (project.Name == null || !project.Name.Any())
+            if (string.IsNullOrEmpty(project.Name))
             {
-                errors.Add("No Project Name Identified");
+                errors.Add(Constants.NoProjNameFound);
             }
         }
-
-        // Code review and correction required
         private void ValidateProjectReference(Project project)
         {
-            if (project.Reference == null || !project.Reference.Any())
+            if (string.IsNullOrEmpty(project.Reference))
             {
-                errors.Add("No Project Reference Identified");
+                errors.Add(Constants.NoProjReferenceFound);
             }
         }
-
-        // Code review and correction required
         private void ValidateProjectStartDate(Project project)
         {
             if (project.StartDate == DateTime.MinValue)
             {
-                 errors.Add("No Project StartDate Identified");
+                errors.Add(Constants.NoProjStartDateFound);
             }
         }
-
-        //valid
         private void ValidateProjectFieldingPeriod(Project project)
         {
             if (project.FieldingPeriod <= 0)
             {
-                errors.Add("No Project FieldingPeriod Identified");
+                errors.Add(Constants.NoProjFieldingPeriodFound);
             }
         }
-
-        //valid
         private void ValidateProjectCategories(Project project)
         {
             if (project.Categories == null || !project.Categories.Any())
-            {               
-                errors.Add("No Categories Identified");
+            {
+                errors.Add(string.Format(Constants.NoCategoryFound, project.Id));
             }
         }
-
-        // Code review and correction required
         private void ValidateProjectUser(Project project)
         {
-            // !!?!?!?!?!!?!!?!
-            if (project.User == null)
-            {           
-            if (project.User.Name == null)               
-                errors.Add("No Project User Name Identified");
-            if (project.User.Email == null)
-                errors.Add("No Project User Email Identified");            
+            if (project.User != null)
+            {
+                if (string.IsNullOrEmpty(project.User.Name))
+                    errors.Add(Constants.ProjUserNameErr);
+                if (!ValidateEmail(project.User.Email))
+                    errors.Add(Constants.ProjUserEmailErr);               
             }
+            else
+                errors.Add(string.Format(Constants.NoUserFound, project.Id));
         }
-
         private bool ValidateUrl(string url)
         {
             if (string.IsNullOrEmpty(url))
@@ -119,123 +93,144 @@ namespace IntelligentSampleEnginePOC.API.Core.Services
 
             Uri validatedUri;
 
-            if (Uri.TryCreate(url, UriKind.Absolute, out validatedUri)) //.NET URI validation.
+            if (Uri.TryCreate(url, UriKind.Absolute, out validatedUri))
             {
-                //If true: validatedUri contains a valid Uri. Check for the scheme in addition.
                 return (validatedUri.Scheme == Uri.UriSchemeHttp || validatedUri.Scheme == Uri.UriSchemeHttps);
             }
             return false;
         }
 
+        private bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            if (new EmailAddressAttribute().IsValid(email))
+                return true;
+            else
+                return false;
+        }
         private void ValidateProjectAsTA(Project project)
         {
             if (project.TargetAudiences != null && project.TargetAudiences.Any())
             {
-                foreach(var item in project.TargetAudiences)
+                var targetAudiencesUniqueName = project.TargetAudiences.GroupBy(i => i.Name).Any(g => g.Count() > 1);
+                if (targetAudiencesUniqueName)
+                    errors.Add(Constants.DuplicateTAName);
+                var targetAudiencesUniqueOrder = project.TargetAudiences.GroupBy(i => i.AudienceNumber).Any(g => g.Count() > 1);
+                if (targetAudiencesUniqueName)
+                    errors.Add(Constants.DuplicateTAOrder);
+
+                foreach (var ta in project.TargetAudiences)
                 {
-                    if (string.IsNullOrEmpty(item.Name))
+                    if (string.IsNullOrEmpty(ta.Name))
                         errors.Add(Constants.TANameErr);
-                    if (item.AudienceNumber < 1)
+                    if (ta.AudienceNumber < 1)
                         errors.Add(string.Format(Constants.MinValCheckErr, "Target Audience number (order)", 1));
-                    if (item.EstimatedIR < 1)
+                    if (ta.EstimatedIR < 1)
                         errors.Add(string.Format(Constants.MinValCheckErr, "Estimated IR", 1));
-                    if (item.EstimatedLOI < 1)
+                    if (ta.EstimatedLOI < 1)
                         errors.Add(string.Format(Constants.MinValCheckErr, "Estimated LOI", 1));
-                    if (item.Limit < 1)
+                    if (ta.Limit < 1)
                         errors.Add(string.Format(Constants.MinValCheckErr, "Wanted Completes (or Limit)", 1));
-                    if (!ValidateUrl(item.TestingUrl))
+                    if (!ValidateUrl(ta.TestingUrl))
                         errors.Add(string.Format(Constants.InvalidUrlErr, "Testing Url"));
-                    if(!ValidateUrl(item.LiveUrl))
+                    if (!ValidateUrl(ta.LiveUrl))
                         errors.Add(string.Format(Constants.InvalidUrlErr, "Live Url"));
-                    if(item.Qualifications != null && item.Qualifications.Count > 1)
+                    if (ta.Qualifications != null && ta.Qualifications.Count > 1)
                     {
-                        if (!item.Qualifications.Any(x => (x.Question!= null && x.Question.Name == "Country")))
+                        if (!ta.Qualifications.Any(x => (x.Question != null && x.Question.Name == "Country")))
                             errors.Add(Constants.QualValidationMessage);
                     }
                     else
                     {
                         errors.Add(Constants.QualValidationMessage);
                     }
+                    if (ta.Qualifications != null && ta.Qualifications.Any())
+                    {
+                        foreach (Qualification qual in ta.Qualifications)
+                        {                            
+                            if (qual.Order <1)
+                                errors.Add(string.Format(Constants.MinValCheckErr, "Qualification Id", 1));
+                           
+                            if (qual.Question != null)
+                            {                               
+                                if (qual.Question.Id <1)
+                                    errors.Add(string.Format(Constants.MinValCheckErr, "Qualification Question Id", 1));
+                                if (string.IsNullOrEmpty(qual.Question.Name))
+                                    errors.Add(string.Format(Constants.NoQualQuestionNameFound, qual.Id));
+                                if (string.IsNullOrEmpty(qual.Question.Text))
+                                    errors.Add(string.Format(Constants.NoQualQuestionTextFound, qual.Id));
+                                if (string.IsNullOrEmpty(qual.Question.CategoryName))
+                                    errors.Add(string.Format(Constants.NoQualQuestionCategoryFound, qual.Id));
 
-                    //If required, validate each qualification item fields here
-                    // foreach(var subItem in item.Qualifications) { ValidateForQualification(subItem);  }
+                                if (qual.Question.Variables != null && qual.Question.Variables.Count > 0)
+                                {
+                                    foreach (var variable in qual.Question.Variables)
+                                    {
+                                        if (variable.Id < 1)
+                                            errors.Add(string.Format(Constants.MinValCheckErr, "VarId", 1));
+                                        if (string.IsNullOrEmpty(variable.Name))
+                                            errors.Add(string.Format(Constants.NoQualVariableNameFound, qual.Id));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        errors.Add(string.Format(Constants.NoQualificationFound, ta.Id));
+                    }
+                    if (ta.Quotas != null && ta.Quotas.Count > 1)
+                    {
+                        foreach (Quota qa in ta.Quotas)
+                        {
+                            var quotaName = ta.Quotas.GroupBy(x => x.QuotaName).Any(g => g.Count() > 1);
+                            if (quotaName)
+                                errors.Add(Constants.DuplicateQuotaName);                            
+                            if (string.IsNullOrEmpty(qa.QuotaName))
+                                errors.Add(string.Format(Constants.NoQuotaNameFound, qa.Id));                            
+                             if (qa.FieldTarget < 1)
+                                 errors.Add(string.Format(Constants.MinValCheckErr, "Quota FieldTarget", 1));
+                             if (qa.Limit < 1)
+                                 errors.Add(string.Format(Constants.MinValCheckErr, "Quota Limit", 1));                            
 
-                    //if required, valid each quota item fields here
-                    // foreach(var subItem in item.Quotas) { ValidateForQuotas(subItem);  }
+                            if (qa.Conditions!=null && qa.Conditions.Count > 0)
+                            {
+                                foreach (Question qs in qa.Conditions)
+                                {
+                                    if (qs.Id < 1)
+                                        errors.Add(string.Format(Constants.MinValCheckErr, "Quota Question Id", 1));
+                                    if (string.IsNullOrEmpty(qs.Name))
+                                        errors.Add(string.Format(Constants.NoQuestionNameFound, qs.Id));
+                                    if (string.IsNullOrEmpty(qs.Text))
+                                        errors.Add(string.Format(Constants.NoQuestionTextFound, qs.Id));
+                                    if (string.IsNullOrEmpty(qs.CategoryName))
+                                        errors.Add(string.Format(Constants.NoQuestionCategoryFound, qs.Id));
+                                    if (qs.Variables != null && qs.Variables.Count > 0)
+                                    {
+                                        foreach (Variable v in qs.Variables)
+                                        {
+                                            if (v.Id < 1)
+                                                errors.Add(string.Format(Constants.MinValCheckErr, "VarId", 1));
+                                            if (string.IsNullOrEmpty(v.Name))
+                                                errors.Add(string.Format(Constants.NoQuestionVariableNameFound, qs.Id));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        errors.Add(string.Format(Constants.NoQuotaFound, ta.Id));
+                    }
                 }
             }
             else
                 errors.Add(string.Format(Constants.NoTAFound, project.Id));
-
-        }
-
-        // Code review and correction required
-        private void ValidateProjectAsTAInvalid(Project project)
-        {
-            if (project.TargetAudiences == null || !project.TargetAudiences.Any())
-            {
-               errors.Add("No TargetAudiences Identified");               
-            }
-            if (project.TargetAudiences.Count > 0 && project.TargetAudiences != null)
-            {
-                foreach (TargetAudience ta in project.TargetAudiences)
-                {
-                    if (ta.Name == null)
-                        errors.Add("No TargetAudiences Name Identified");
-                    if (ta.AudienceNumber <= 0)
-                        errors.Add("No TargetAudiences Number Identified");
-                    if (ta.EstimatedIR <= 0)
-                        errors.Add("No TargetAudiences EstimatedIR Identified");
-                    if (ta.EstimatedLOI <=0)
-                        errors.Add("No TargetAudiences EstimatedLOI Identified");
-                    /*if (ta.LimitType == null)
-                        errors.Add("No TargetAudiences LimitType Identified");*/
-                    if (ta.TestingUrl == null)
-                        errors.Add("No TargetAudiences TestingUrl Identified");
-                    if (ta.LiveUrl == null)
-                        errors.Add("No TargetAudiences LiveUrl Identified");
-                  foreach (Qualification qual in ta.Qualifications)
-                    {   
-                        var qualificationCountcheck = ta.Qualifications.Count > 1;
-                        if (!qualificationCountcheck)
-                            errors.Add(Constants.QUALIFICATIONCOUNTERCHECKSTART + ta.Id + Constants.QUALIFICATIONCOUNTERCHECKEND);                       
-                            if (qual.Order <= 0)
-                                errors.Add(Constants.QORDER + ta.Id + Constants.TANAME + ta.Name);
-                            if (qual.LogicalDecision == null)
-                                errors.Add(Constants.QLOGICALDICISION + ta.Id + Constants.TANAME + ta.Name);
-                            if (qual.NumberOfRequiredConditions <= 0)
-                                errors.Add(Constants.QNOOFREQUIREDCONDITIONS  + ta.Id + Constants.TANAME + ta.Name);
-                            if (!qual.IsActive == false || !qual.IsActive == true)
-                                errors.Add(+ ta.Id + Constants.TANAME + ta.Name);
-                            if (qual.Question != null)
-                            {
-                                var qualificationCountryCheck = ta.Qualifications.Any(i => i.Question.Name == "Country");
-                                if (!qualificationCountryCheck)
-                                    errors.Add(Constants.QUALIFICATIONCOUNTRYCHECK + ta.Id + Constants.QID + qual.Id);
-                                if (qual.Question.Id <= 0)
-                                    errors.Add(Constants.QQUESTIONID + qual.Id);
-                                if (qual.Question.Name == null)
-                                    errors.Add(Constants.QQUESTIONNAME + qual.Id);
-                                if (qual.Question.Text == null)
-                                    errors.Add(Constants.QQUESTIONTEXT + qual.Id);
-                                if (qual.Question.CategoryName == null)
-                                    errors.Add(Constants.QCATEGORYNAME + qual.Id);
-                            //!!!?!?!?!?!?    
-                            if (qual.Question.Variables == null && qual.Question.Variables.Count > 0)
-                                {
-                                    foreach (var variable in qual.Question.Variables)
-                                    {
-                                        if (variable.Id <= 0)
-                                            errors.Add(Constants.VARIABLEID + qual.Id);
-                                        if (variable.Name == null)
-                                            errors.Add(Constants.VARIABLENAME + qual.Id);
-                                    }
-                                }
-                            }
-                    }
-                }
-            }
         }
 
     }
 }
+
